df9b5577beef4569d9801bc22ddefdf50eaa4ef7
allemus06 <allemus06@yahoo.com>
2017-10-11 15:58:12 -0700

    Initial commit

---
+++ app/src/main/java/cecs453/android/csulb/edu/recipeapp/MainActivity.java
@@ -0,0 +1,13 @@
+package cecs453.android.csulb.edu.recipeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
